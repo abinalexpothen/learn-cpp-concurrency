98836f35a8acacd8c2a2da8ae7a92b7602b2cc27
Abin Alex Pothen <abinalexpothen@gmail.com>
2026-02-21 00:07:51 -0500

    explore some experiments with thread lifetime issues and RAII idiom for joining threads

--- learn/thread-management/launch_a_thread.cpp
+++ learn/thread-management/launch_a_thread.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 void do_something()
 {
     std::cout << "\n Do something amazing!";
+}
+
+void do_something_with(int i)
+{
+    std::cout << "I got " << i << std::endl; // <-- end forces immediate flush of buffer
 }
 
 class background_task
@@ -16,6 +22,36 @@ public:
     }
 };
 
+// an example lifetime issue test
+struct lifetime
+{
+    int &i;
+
+    lifetime(int &i_) : i(i_) {}
+
+    void operator()()
+    {
+        for (unsigned j = 0; j < 5; ++j)
+        {
+            do_something_with(i);
+        }
+    }
+
+    void lifetimetest()
+    {
+        for (unsigned j = 0; j < 10; ++j)
+        {
+            std::cout << "\n Number is " << j;
+        }
+    }
+};
+
+void do_something_in_here_and_throw_excpetion()
+{
+    std::vector<int> v;     // empty vector
+    std::cout << v.at(100); // access 100th element of empty vector, causes a standard exception
+}
+
 int main()
 {
     background_task f;
@@ -25,7 +61,7 @@ int main()
     std::thread t2(f);
 
     // this doesn't start a thread. It declared a t3 function that takes a single param  (of type pointer to function taking no params and returning background_task object
-    std::thread t3(background_task()); // <- compiler shows a warning
+    // std::thread t3(background_task()); // <- compiler shows a warning
 
     // but, these do start a thread
     std::thread t4{background_task()};   // using uniform init syntax
@@ -41,6 +77,31 @@ int main()
     t4.join();
     t5.join();
     t6.join();
+
+    std::cout << "\n Demonstrate lifetime issues";
+    int some_local_state = 0;
+    lifetime my_lifetime(some_local_state);
+    std::thread my_thread(my_lifetime);
+    // my_thread.detach();
+    std::cout << "\n Is thread joinable now? -> " << my_thread.joinable(); // <-this will cause interlaced cout display as both threads are printing to same resource
+    my_thread.join();
+    std::cout << "\n Is thread joinable now? -> " << my_thread.joinable() << "\n";
+
+    // join threads even in the case of exceptions tp prevent accidental lifetime problems
+    int another_local_var = 2;
+    lifetime my_secondlifetime(another_local_var);
+    std::thread my_second_thread(my_secondlifetime);
+    try
+    {
+        do_something_in_here_and_throw_excpetion();
+    }
+    catch (...)
+    {
+        my_second_thread.join();
+        throw;
+    }
+
+    my_second_thread.join();
 
     return 0;
 }
---
+++ learn/thread-management/thread_with_raii.cpp
@@ -0,0 +1,80 @@
+// it's important to ensure that thread joins for any exit condition, whether through exception or otherwise; use RAII
+// use join() in the destructor
+
+#include <thread>
+#include <iostream>
+#include <vector>
+
+void do_something(int i)
+{
+    std::cout << "I am doing something with " << i << std::endl; // <-- i want to flush buffer immediately, so endl
+}
+
+void do_something_with_std_exception()
+{
+    std::vector<int> v;
+    v.at(100); // exception because vector is empty
+}
+
+class thread_guard
+{
+    std::thread &t;
+
+public:
+    explicit thread_guard(std::thread &t_) : t(t_) {}
+
+    ~thread_guard()
+    {
+        if (t.joinable())
+        {
+            t.join();
+        }
+        std::cout << "Called destructor of thread_guard" << std::endl;
+    }
+
+    // copying or assigning is are deleted to prevent the object from
+    // outliving the scope of the thread it was joining
+    thread_guard(thread_guard const &) = delete;
+    thread_guard &operator=(thread_guard const &) = delete;
+};
+
+struct f
+{
+    int &i;
+    f(int &i_) : i(i_) {}
+
+    void operator()()
+    {
+        for (unsigned j = 0; j < 100000; j++)
+        {
+            do_something(i);
+        }
+    }
+};
+
+void problem_function()
+{
+    // even through the motivation to RAII in this context was to not use the try/catch,
+    // it looks like the std::exception causes std::terminate to be called when there's no
+    // try-catch for it.
+    try
+    {
+        int some_local_state = 0;
+        f my_function(some_local_state);
+        std::thread t(my_function);
+        thread_guard g(t);
+        // note: local objects are destroyed in reverse order of construction,
+        // so the destructor of thread_guard will be hit first; it has a valid t to join()
+        do_something_with_std_exception();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Caught in main: " << e.what() << std::endl;
+    }
+}
+
+int main()
+{
+    problem_function();
+    return 0;
+}
